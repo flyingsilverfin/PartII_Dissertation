eb4f383d3001e841bd3b22f188fa5e1f9ec64e92
Joshua Send <joshuasend1@gmail.com>
2016-11-15 15:24:08 +0000

    removed accidental js files

--- CRDT/src/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Client_1 = require('../modules/Client');
-var NetworkManager_1 = require('../modules/NetworkManager');
-var nm = new NetworkManager_1["default"]();
-for (var i = 0; i < 2; i++) {
-    new Client_1["default"](nm);
-}
